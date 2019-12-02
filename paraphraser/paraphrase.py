5dca531bb4089b9f017f6857f5494bc353ed6f1b
myxikpupsik <myxikpupsik@gmail.com>
2019-12-02 17:22:55 +0600

    Sungbae, zakashi edu

--- paraphraser/paraphrase.py
+++ paraphraser/paraphrase.py
@@ -6,7 +6,7 @@ from lstm_model import lstm_model
 import numpy as np
 from pprint import pprint as pp
 from inference import Paraphraser
-
+from synonyms import replace_with_syn
 
 def paraphrase(sentence):
 	"""Outputs a paraphrased sentence str. 
@@ -18,16 +18,16 @@ def paraphrase(sentence):
 		paraphrases = p.sample_paraphrase(sentence, sampling_temp=0.1*(i+1), how_many=100)
 		for j, p_sentence in enumerate(paraphrases):
 			try:
-				if p_sentence.lower().split(' ').remove('.')==sentence.lower().split(' '):
+				repl = replace_with_syn(p_sentence)
+				if repl.lower().split(' ').remove('.')==sentence.lower().split(' '):
 					continue
 				else:
-					return p_sentence
+					tripl_p = p.sample_paraphrase(repl, sampling_temp=0.1, how_many=1)
+					return repl if tripl_p.lower().split(' ').remove('.')==sentence.lower().split(' ') else tripl_p
 			except ValueError:
-				if p_sentence.lower().split(' ')==sentence.lower().split(' '):
+				repl = replace_with_syn(p_sentence)
+				if repl.lower().split(' ')==sentence.lower().split(' '):
 					continue
 				else:
-					return p_sentence
-
-
-inpt = input()
-print(paraphrase(inpt))
+					tripl_p = p.sample_paraphrase(repl, sampling_temp=0.1, how_many=1)
+					return repl if tripl_p.lower().split(' ')==sentence.lower().split(' ') else tripl_p
